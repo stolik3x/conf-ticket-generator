f2298d178f812a17ebc18c863ad84c297e651ea4
stolik3x <rebsdebs@gmail.com>
2025-02-05 15:20:48 +0100

    Изменил билет сайт

--- js/main.js
+++ js/main.js
@@ -1,33 +1,24 @@
 document.getElementById("avatar").addEventListener("change", function(event) {
-  const file = event.target.files[0]; // Получаем загруженный файл
+  const file = event.target.files[0];
 
   if (file) {
-    // Проверяем размер файла
-    const maxFileSize = 500 * 1024; // 500 KB в байтах
+    const maxFileSize = 500 * 1024;
     if (file.size > maxFileSize) {
-      alert("File size exceeds 500KB. Please upload a smaller image.");
-      return; // Прерываем выполнение функции, если файл слишком большой
+      alert("Please upload a smaller image.");
+      return;
     }
 
     const reader = new FileReader();
 
     reader.onloadend = function() {
-      const base64Image = reader.result; // Получаем строку в формате base64
-
-      // Сохраняем base64 строку изображения в localStorage
+      const base64Image = reader.result;
       localStorage.setItem("userAvatar", base64Image);
-
-      // Показываем контейнер с превью
       document.getElementById("previewContainer").style.display = "block";
-
-      // Отображаем картинку в разделе загрузки
       document.getElementById("avatarPreview").src = base64Image;
-
-      // Показываем сообщение, что изображение загружено
       document.getElementById("uploadStatus").textContent = "Image uploaded successfully!";
     };
 
-    reader.readAsDataURL(file); // Читаем файл как base64 строку
+    reader.readAsDataURL(file);
   }
 });
 
@@ -41,37 +32,32 @@ document.getElementById("submitButton").addEventListener("click", function(event
   const name = document.getElementById("nameArea").value;
   const email = document.getElementById("mailArea").value;
   const github = document.getElementById("gitArea").value;
-  const avatar = localStorage.getItem("userAvatar"); // Получаем аватар из localStorage
+  const avatar = localStorage.getItem("userAvatar");
 
   let formIsValid = true;
 
-  // Проверка на имя
   if (name.trim() === "") {
     document.getElementById("nameErr").textContent = "Full name is required.";
     formIsValid = false;
   }
 
-  // Проверка на email
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   if (!email.match(emailPattern)) {
     document.getElementById("mailErr").textContent = "Please enter a valid email address.";
     formIsValid = false;
   }
 
-  // Проверка на GitHub
   const githubPattern = /^[a-zA-Z0-9@]+$/;
   if (!github.match(githubPattern)) {
     document.getElementById("gitErr").textContent = "GitHub username should contain only letters and numbers.";
     formIsValid = false;
   }
 
-  // Проверка на загрузку аватара
   if (!avatar) {
     alert("Please upload an avatar before submitting the form.");
     formIsValid = false;
   }
 
-  // Если форма валидна
   if (formIsValid) {
     localStorage.setItem("userName", name);
     localStorage.setItem("userEmail", email);
